c5247ea72bb63c6c806f5423871f9933e7b06b42
Joseph Cajida <josephdalebertcajida@gmail.com>
2026-05-16 09:22:28 +0800

    change to localhost

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = "http://localhost:5000";
-const API_BASE_URL = "/api";
+const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL = "/api";
 
 export async function apiFetch<T>(
   url: string,
